fc94dd32b8557c4ee2d94c69c9d0d79451f260e4
Adi Saripuloh <adisaripuloh@gmail.com>
2022-04-14 06:45:25 +0700

    remove interface

--- cache/cache.go
+++ cache/cache.go
@@ -1,22 +1,10 @@
 package cache
 
-import (
-	"context"
-	"time"
-)
-
-type (
-	Config struct {
-		Scheme   string `yaml:"scheme"`
-		Host     string `yaml:"host"`
-		Port     int    `yaml:"port"`
-		Username string `yaml:"username"`
-		Password string `yaml:"password"`
-		Database int    `yaml:"database"`
-	}
-	Cacher interface {
-		Set(ctx context.Context, key string, value any, ttl time.Duration) error
-		Get(ctx context.Context, key string, dest any) error
-		Del(ctx context.Context, key []string) error
-	}
-)
+type Config struct {
+	Scheme   string `yaml:"scheme"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Database int    `yaml:"database"`
+}
